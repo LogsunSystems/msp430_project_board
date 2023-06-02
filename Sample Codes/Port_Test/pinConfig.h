bd2d866318ba854622fa7846d524a0791b14f15b
LogsunSystems <108651919+LogsunSystems@users.noreply.github.com>
2023-06-02 12:53:49 +0530

    Simple code for Port Test is added

---
+++ Sample Codes/Port_Test/pinConfig.h
@@ -0,0 +1,45 @@
+#ifndef PINCONFIG_H
+#define PINCONFIG_H
+
+//SV1
+#define P2_0  19  
+#define P7_4  17
+#define P2_6  13
+#define P2_3  12
+#define P8_1  11
+#define P8_2  31
+#define P3_7  32
+#define P3_6  29
+
+//SV2
+#define P2_5  40
+#define P2_4  39
+#define P1_5  38
+#define P1_4  37
+#define P1_3  36
+#define P1_2  35
+#define P4_3  34
+#define P4_0  33
+
+//SV3
+#define P6_0  23
+#define P6_1  24
+#define P6_2  25
+#define P6_3  26
+#define P6_4  27
+#define P6_5  2
+#define P6_6  6   //Can't use as OUTPUT
+#define P7_0  28
+
+//UEXT
+#define P3_3  4
+#define P3_4  3
+#define P4_2  9
+#define P4_1  10
+#define P3_0  15
+#define P3_1  14
+#define P3_2  7
+#define P2_2  18
+
+
+#endif
